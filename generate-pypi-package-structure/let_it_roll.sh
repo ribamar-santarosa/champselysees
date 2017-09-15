fb8f23daeed1030e38b0aa2a86baa7e84d79ce02
Ribamar Santarosa <ribamar.santarosa@brightcomputing.com>
2017-09-15 13:24:28 +0200

    generate-pypi-package-structure/let_it_roll.sh: add troubleshooting tool

--- generate-pypi-package-structure/let_it_roll.sh
+++ generate-pypi-package-structure/let_it_roll.sh
@@ -53,5 +53,41 @@ EOF
 git tag ${my_package_version} -m "tag ${my_package_version} for generation of tar.gz"
 
 file=${arbitrary_root_dirname}  ; git add $file ; git commit -m "$file: added initial structure for pypi"  
+
+
+cat <<  EOF > troubleshooting_${my_package_name}.py
+import sys
+import imp
+E = BaseException
+try:
+  import ${my_package_name}
+except E, e:
+  print("${my_package_name} not imported")
+print('system paths are:')
+print(sys.path)
+imp.load_source('${my_package_name}', '/usr/local/lib/python2.7/dist-packages/pychampselysees/${my_package_name}.py') # see correct location with pip show  ${my_package_name} output
+
+import ${my_package_name}
+${my_package_name}.printIfTrue('imp.load_source loads ${my_package_name}. package is working.')
+print('imp.find_module(${my_package_name}):')
+print(imp.find_module('${my_package_name}'))
+
+del  ${my_package_name}
+try:
+  ${my_package_name}.printIfTrue('package is deleted so, this must raise exception')
+except E, e:
+  print('package is deleted so, this exception is correct')
+  print("${my_package_name} not imported")
+
+import ${my_package_name}
+try:
+  ${my_package_name}.printIfTrue('package is import so, this must NOT raise exception -- this message print means success')
+except E, e:
+  print('package is deleted so, this exception is incorrect')
+  print("${my_package_name} not imported")
+
+EOF
+
+
 popd
 

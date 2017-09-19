e7d0edb3e74f7586f55c299828011da1285324c6
ribamar <ribamar@gmail.com>
2017-09-19 22:42:17 +0200

    linux-reconf: added

---
+++ linux-reconf/reconf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# reconfigure my keyboard:
+setxkbmap -model pc105 -layout us -variant alt-intl # this worked for revolvedeb910, but of course the 
+sudo ibus-daemon --xim
+setxkbmap -model pc105 -layout us -variant alt-intl # this worked for revolvedeb910, but of course the 
+

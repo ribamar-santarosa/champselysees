aeb6f148772fd0f1e39ce7eb57a2a96d94b59c67
Ribamar Santarosa <ribamar.santarosa@brightcomputing.com>
2017-09-20 17:10:19 +0200

    fsforay: fsforay.h: default_bool: added

--- fsforay/fsforay.h
+++ fsforay/fsforay.h
@@ -8,6 +8,7 @@ namespace filesystem_dep = boost::filesystem; // tomorrow boost:: will be replac
 #define default_container std::deque // later a better approach can be defined
 typedef std::string default_string;
 typedef int  default_int;
+typedef bool default_bool;
 typedef filesystem_dep::path default_path;
 
 

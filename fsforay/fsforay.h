26686f018374e7d10e499d6cb27f8753cb5f9b54
Ribamar Santarosa <ribamar.santarosa@brightcomputing.com>
2017-09-22 13:19:00 +0200

    fsforay fsforay.h:  derive_string implementation

--- fsforay/fsforay.h
+++ fsforay/fsforay.h
@@ -2,10 +2,21 @@
 #include <deque>
 #include <vector>
 #include <memory>
+#include <regex>
+#include <map>
 #include "boost/filesystem.hpp"
+#include <boost/algorithm/string/replace.hpp>
+
 
 namespace filesystem_dep = boost::filesystem; // tomorrow boost:: will be replaced by std::
-#define default_container std::deque // later a better approach can be defined
+/* later a better approach can be defined for template types: */
+#define default_container std::deque 
+/*
+ weirdly, I can't define default_ordered_multimap -- it won't compile.
+#define default_ordered_multimap std::multimap
+ so I define as "manymap" instead
+*/
+#define default_ordered_manymap std::multimap
 typedef std::string default_string;
 typedef int  default_int;
 typedef bool default_bool;

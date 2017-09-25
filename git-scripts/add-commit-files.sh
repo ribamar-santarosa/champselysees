6a9a3f3e86b06b2ea88c96f93b6d8a74acc51994
Ribamar Santarosa <ribamar.santarosa@brightcomputing.com>
2017-09-25 13:01:23 +0200

    git-scripts: add-commit-files.sh:  improve comments

--- git-scripts/add-commit-files.sh
+++ git-scripts/add-commit-files.sh
@@ -1,6 +1,7 @@
 # example:
-# add-commit-files.sh "$(ls)" "$(basename $PWD)"  "file added" # add all files in this project
-# add-commit-files.sh "$(ls)" "$(basename $PWD)"  "file changed" "-p" # interactive change all files in this project
+# add-commit-files.sh "$(ls)" "$(basename $PWD):"  "file added" # add all files in this project
+# add-commit-files.sh "$(ls)" "$(basename $PWD):"  "file changed" "-p" # interactive change all files in this project
+# add-commit-files.sh "$(git status | grep modified: | cut -f 2 -d :  | xargs )"  "$(basename $PWD):"  "file changed" "-p" # interactive change all files in this project, same as above, but changed files only
 [[ ! -z "$1" ]] && files=$1
 [[ ! -z "$2" ]] && prefix=$2
 [[ ! -z "$3" ]] && message=$3

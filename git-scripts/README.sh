0a73b0d8d2ac50588cb384057a991d18701b2a6b
Ribamar Santarosa <ribamar.santarosa@brightcomputing.com>
2017-10-03 16:38:42 +0200

    git-scripts:README.sh:  dry-run

--- git-scripts/README.sh
+++ git-scripts/README.sh
@@ -7,5 +7,6 @@ usage:
 ./add-commit-files.sh "$(ls)" "$(basename $PWD):"  "file added" # add all files in this project/this dir
 ./add-commit-files.sh "$(ls)" "$(basename $PWD):"  "file changed" "-p" # interactive change all files in this project
 ./add-commit-files.sh "$(git status | grep modified: | cut -f 2 -d :  | xargs )"  "$(basename $PWD):"  "file changed" "-p" # interactive change all files in this project, same as above, but changed files only
+./add-commit-files.sh "$(ls)" "$(basename $PWD):"  "file changed" "-p" "echo" # echo git commands instead
 
 ````

a5bf0152dcc2917e2a54ae2afc167cad809c878b
ribamar <ribamar@gmail.com>
2018-02-28 13:11:33 +0100

    bashement: bashement-env.sh:  bm_champselysees_install: added

--- bashement/bashement-env.sh
+++ bashement/bashement-env.sh
@@ -9,3 +9,7 @@ export bm_db_password=
 # function: bm_git_clone_and_pull
 export bm_repo_url=
 export bm_repo_dir=
+
+# function: bm_champselysees_install
+export bm_champselysees_repo_dir="${HOME}/.champselysees"
+export bm_champselysees_repo_url="https://github.com/ribamar-santarosa/champselysees.git"

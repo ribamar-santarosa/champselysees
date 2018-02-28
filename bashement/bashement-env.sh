c1def7150945cee96e7fa8515920656946725866
ribamar <ribamar@gmail.com>
2018-02-28 13:09:15 +0100

    bashement: bashement-env.sh:  bm_git_clone_and_pull: added

--- bashement/bashement-env.sh
+++ bashement/bashement-env.sh
@@ -5,3 +5,7 @@ export bm_db_name=bm_database
 export bm_db_user=$(whoami)
 export bm_db_host=$(hostname)
 export bm_db_password=
+
+# function: bm_git_clone_and_pull
+export bm_repo_url=
+export bm_repo_dir=

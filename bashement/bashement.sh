21eb295e6f05ca4e8577cf4a3e502c1bba4aa003
ribamar <ribamar@gmail.com>
2018-02-28 16:09:22 +0100

    bashement: bashement.sh:   bm_install_itself : comments: improved

--- bashement/bashement.sh
+++ bashement/bashement.sh
@@ -128,8 +128,9 @@ function bm_source_newest_env {
 
 # bm_install_itself
 # downloads the newest version of this script,
-# sources it and
-# installs champelysees
+# sources it and installs champelysees.
+# note that it requires a previously env
+# to be loaded.
 # * expects:
 # bm_bashement_raw_url,
 # bm_bashement_path

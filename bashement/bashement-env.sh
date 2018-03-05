cef094faa579163ac70bfb78f745dff401289a72
ribamar <ribamar@gmail.com>
2018-03-05 17:08:29 +0100

    bashement: bashement-env.sh:   bm_source_env: added

--- bashement/bashement-env.sh
+++ bashement/bashement-env.sh
@@ -6,7 +6,7 @@ export bm_time=$(date +"%Y.%m.%d_%H:%M:%S")  #  timestamp
 export bm_out_prefix=bm_${time}
 
 
-# function: bm_source_newest_env
+# function: bm_source_newest_env, bm_source_env
 export bm_bashement_env_raw_url=
 export bm_bashement_env_path=
 

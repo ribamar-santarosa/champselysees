98a9d5662582a024cf4892c038e322c2bd6698a7
ribamar <ribamar@gmail.com>
2018-03-07 14:32:25 +0100

    bashement: bashement.sh:   bm_psql_apply_dump: improved: uses: bm_psql_query: comments

--- bashement/bashement.sh
+++ bashement/bashement.sh
@@ -521,7 +521,8 @@ function bm_psql_query {
 # * (over)writes:
 # database bm_db_name in psql,
 # file bm_out_psql_restore,
-# bm_out_psql_restore_query
+# bm_db_query,
+# bm_psql_query_out_file
 # PGPASSWORD
 #
 function bm_psql_apply_dump {

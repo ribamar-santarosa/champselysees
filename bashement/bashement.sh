4f6d88a123b549d8085a83ac9f11663f11e9f5f7
ribamar <ribamar@gmail.com>
2018-03-07 15:42:54 +0100

    bashement: bashement.sh:   var bm_db_dump_file: usage: quoted

--- bashement/bashement.sh
+++ bashement/bashement.sh
@@ -470,7 +470,7 @@ function bm_fs_wipe_swp {
 # PGPASSWORD
 #
 function bm_psql_generate_dump {
-  [[  -f  "${bm_db_dump_file}"   ]] &&   echo "${bm_db_dump_file} exists" || (   ( PGPASSWORD="${bm_db_password}"  pg_dump  -h  ${bm_db_host} -U ${bm_db_user} ${bm_db_name}  )  |  tee  ${bm_db_dump_file} )     #iffileexists
+  [[  -f  "${bm_db_dump_file}"   ]] &&   echo "${bm_db_dump_file} exists" || (   ( PGPASSWORD="${bm_db_password}"  pg_dump  -h  ${bm_db_host} -U ${bm_db_user} ${bm_db_name}  )  |  tee  "${bm_db_dump_file}" )     #iffileexists
   echo "Generated (I hope) dump file, if it didn't exist." &> /dev/stderr
   echo "For restoring it (later), export the following var:" &> /dev/stderr
   echo "export bm_db_dump_file='${bm_db_dump_file}'" &> /dev/stderr

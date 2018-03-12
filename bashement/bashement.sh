d8d1daed666544ab3769f7103d56ed07ae139f1b
ribamar <ribamar@gmail.com>
2018-03-12 18:19:41 +0100

    bashement: bashement.sh:   function bm_resolve: changed: uses: bm_fallback_echo_command

--- bashement/bashement.sh
+++ bashement/bashement.sh
@@ -534,6 +534,7 @@ function bm_export_prepare_var {
 # bm_resolve_tmp_var
 #
 function bm_resolve {
+  bm_fallback_echo_command
   export bm_resolve_tmp_var=$(${bm_echo_command} ${bm_resolve_var})
   ${bm_echo_command} ${!bm_resolve_tmp_var}
 

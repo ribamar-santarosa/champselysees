227ee90ff505e2fa32e9990ad3a694792a91003e
ribamar <ribamar@gmail.com>
2018-03-12 16:32:02 +0100

    bashement: bashement.sh:   function bm_git_update_branch: improved: uses: bm_export_git_current_branch

--- bashement/bashement.sh
+++ bashement/bashement.sh
@@ -102,7 +102,7 @@ function bm_git_update_branch {
   export bm_fallback_var="bm_git_reset_and_hard"
   export bm_fallback_to="--hard"
   bm_fallback
-  export bm_git_current_branch=$(git rev-parse --abbrev-ref HEAD)
+  bm_export_git_current_branch
   git checkout ${bm_git_branch_a}
   git checkout ${bm_git_branch_b}  ||  git checkout -b ${bm_git_branch_b}
   export bm_conditioned_run_condition_var="bm_git_reset_at_head"

ecdba3ad249b1faa23403a726361d3d3938b1773
ribamar <ribamar@gmail.com>
2018-03-13 17:45:59 +0100

    bashement: bashement.sh:   bm_assign: fix

--- bashement/bashement.sh
+++ bashement/bashement.sh
@@ -670,9 +670,10 @@ function bm_fallback_echo_command {
 #
 # function: bm_assign
 function bm_assign {
-  export bm_export_var="${bm_assign_var}"
-  export bm_resolve_var="bm_assign_value"
+  export bm_export_var="${bm_assign_to}"
+  export bm_resolve_var="${bm_assign_var}"
   export bm_export_value="$(bm_resolve)"
+
   bm_export
 }
 

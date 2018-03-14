8d8a2f52e0fc8a94fd5b39b1a13b73dfaa956923
ribamar <ribamar@gmail.com>
2018-03-14 12:15:35 +0100

    bashement: bashement.sh:    bm_export_prepare_with_list_shift: comments: fix

--- bashement/bashement.sh
+++ bashement/bashement.sh
@@ -929,7 +929,7 @@ function bm_list_shift {
 }
 
 
-# bm_export_prepare_with_namespace_rm
+# bm_export_prepare_with_list_shift
 # exports a var (given by bm_export_prepare_var)
 # with the results of calling bm_list_shift
 # 

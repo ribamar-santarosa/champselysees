5257f3da651358a058c5c1747ba6000df0581917
ribamar <ribamar@gmail.com>
2018-04-18 15:04:05 +0200

    enc-dec-ruby: dec.rb:  deprecated note

--- enc-dec-ruby/dec.rb
+++ enc-dec-ruby/dec.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/env ruby
 
+=begin
+# Deprecation Note:
+`enc-dec-ruby`: both `dec.rb` and `enc.rb` are outdated in favour of
+`rubyment`'s `shell_dec` and `shell_enc`. Files left for API contract,
+but not a good idea to start using them.
+=end
+
 class ProjectExecution
   require 'openssl'
   require 'stringio'

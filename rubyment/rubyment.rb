74e1f2779bb58788ffaa78ddd0b27d54d528d53c
ribamar <ribamar@gmail.com>
2018-04-19 12:09:00 +0200

    rubyment : rubyment.rb:  function initialize: static_end_key defined

--- rubyment/rubyment.rb
+++ rubyment/rubyment.rb
@@ -18,6 +18,7 @@ class Rubyment
       :stdin  => STDIN,
       :time   => Time.now,
       :static_separator_key => "strings_having_this_string_not_guaranteed_to_work",
+      :static_end_key => "strings_havinng_this_string_also_not_guaranteed_to_work",
     }
     @memory.update memory
     invoke = @memory[:invoke].to_a

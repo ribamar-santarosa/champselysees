504dde81588154d0586c2638eac2381e30cac17d
ribamar <ribamar@gmail.com>
2018-04-30 18:34:11 +0200

    rubyment : rubyment.rb:   function initialize: improved: major_version: updated

--- rubyment/rubyment.rb
+++ rubyment/rubyment.rb
@@ -60,7 +60,7 @@ class Rubyment
       :stdout => STDOUT,
       :stdin  => STDIN,
       :time   => Time.now,
-      :major_version => "0.1",
+      :major_version => "0.2",
       :basic_version => (Time.now.to_i  / 60), # new one every minute
       :filepath => __FILE__,
       :running_dir => Dir.pwd,

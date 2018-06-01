182e8d75b74071e5f935274e34b08df596bb0494
Ribamar Santarosa <ribamar@gmail.com>
2018-06-01 15:00:22 +0200

    rubyment : rubyment.rb:   function initialize: improved: if a string had the source code of rubyment.rb, it wouldn't be guaranteed to work in certain cases

--- rubyment/rubyment.rb
+++ rubyment/rubyment.rb
@@ -67,8 +67,8 @@ class Rubyment
       :home_dir => Dir.home,
       :system_user => ENV['USER'] || ENV['USERNAME'],
       :system_user_is_super => ENV['USER']  == "root", # changed plan: platform indenpend.
-      :static_separator_key => "strings_having_this_string_not_guaranteed_to_work",
-      :static_end_key => "strings_havinng_this_string_also_not_guaranteed_to_work",
+      :static_separator_key => "strings_having_this"  +  "_string_not_guaranteed_to_work",
+      :static_end_key => "strings_havinng_this_string" + "_also_not_guaranteed_to_work",
     }
     @memory.update memory.to_h
     invoke @memory[:invoke].to_a

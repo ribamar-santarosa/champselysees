c56b510be12d1331e99fe67c846974eb6947da24
Ribamar Santarosa <ribamar@gmail.com>
2018-06-05 10:23:52 +0200

    rubyment : rubyment.rb:   function binary_input_single_line_non_echo: fixed (Ruby binary String.b)

--- rubyment/rubyment.rb
+++ rubyment/rubyment.rb
@@ -364,7 +364,7 @@ class Rubyment
     stdin  = @memory[:stdin]
     stderr.print "single line:"
     static_separator_key_per_execution = @memory[:static_separator_key_per_execution]
-    args.shift.to_s.split(static_separator_key_per_execution).first || stdin.gets.chomp
+    args.shift.to_s.b.split(static_separator_key_per_execution).first || stdin.gets.chomp
   end
 
 

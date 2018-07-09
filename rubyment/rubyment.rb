5ef1b7df38952152419ee1f2c89f836bd2892437
Ribamar Santarosa <ribamar@gmail.com>
2018-07-09 18:19:54 +0200

    rubyment : rubyment.rb:   function test__tcp_server_plain: fix (accepts happy_with_request)

--- rubyment/rubyment.rb
+++ rubyment/rubyment.rb
@@ -2097,7 +2097,7 @@ require '#{gem_name}'
 
   # test for tcp_server_plain
   def test__tcp_server_plain args = ARGV
-   thread = tcp_server_plain [8003, "0", "", "debug", "io_echo", "debug_io_echo", "arg_to_io_2"]
+   thread = tcp_server_plain [8003, "0", "", "debug", "io_echo", "debug_io_echo"]
    thread.join
   end
 

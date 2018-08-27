85fc29675710e22ab8ccf74889c7b55384b7c257
Ribamar Santarosa <ribamar@gmail.com>
2018-08-27 11:56:17 +0200

    rubyment : rubyment.rb:   function test__array_unflatten_base: improved (test case base_case_double_inverse: added)

--- rubyment/rubyment.rb
+++ rubyment/rubyment.rb
@@ -4113,6 +4113,18 @@ n8mFEtUKobsK
         ],
       ],
 
+      [
+        "base_case_double_inverse", [ :a, "[", "[", :b, "]", "]", :c], [
+           :array_unflatten_base, [
+             [:a, [[ :b, ]], :c],
+             :shallow.negate_me,
+             :debug.negate_me,
+             :reserved_tokens.to_nil,
+             :inverse,
+           ]
+        ],
+      ],
+
 
     ]
     test__tester test_cases

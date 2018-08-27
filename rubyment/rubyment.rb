986258d7001b2a58dd213356312f00932d7a9c80
Ribamar Santarosa <ribamar@gmail.com>
2018-08-27 11:56:25 +0200

    rubyment : rubyment.rb:   module RubymentUnderDeprecationRuneFunctionsModule: function test__tester: added

--- rubyment/rubyment.rb
+++ rubyment/rubyment.rb
@@ -679,6 +679,26 @@ end
 module RubymentUnderDeprecationRuneFunctionsModule
 
 
+  # generic function for test__ functions
+  def test__tester args=[]
+    expectation = {}
+    actual = {}
+    test_cases = args
+    test_cases ||= [
+     # [ :id, :expectation, :actual_params ],
+    ]
+    test_cases.each_with_index{ |test_case|
+      test_case_id, test_expectation, actual_params = test_case
+      result = send actual_params[0], actual_params[1]
+      expectation[test_case_id] = test_expectation
+      actual[test_case_id] = result
+    }
+    judgement = actual.keys.map {|test_case|
+      [expectation[test_case], actual[test_case] , test_case]
+    }.map(&method("expect_equal")).all?
+  end
+
+
   # creates a Proc out of a block
   def bl *args, &block
     block ||= lambda {}
